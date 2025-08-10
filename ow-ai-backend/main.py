ec04d5724729a96de5bee3766f72e18e6a95d7c9
drking2700 <donald.king@amplifycoast.com>
2025-08-10 11:21:33 -0500

    2frontend analytics upgrade

--- ow-ai-backend/main.py
+++ ow-ai-backend/main.py
@@ -22,7 +22,8 @@ from routes.enterprise_user_management_routes import router as enterprise_user_r
 from routes.authorization_routes import router as authorization_router
 from routes.authorization_routes import authorization_api_router
 from routes.enterprise_secrets_routes import router as secrets_router
-from analytics import router as analytics_router
+from routes.analytics_routes import router as analytics_router
+
 
 
 
