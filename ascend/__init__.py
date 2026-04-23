fb087113ad15b822c9f911e8af31f015269c5cd7
drking2700 <donald.king@amplifycoast.com>
2026-04-22 21:15:34 -0400

    chore(release): bump version to 2.4.3

--- ascend/__init__.py
+++ ascend/__init__.py
@@ -40,10 +40,10 @@ Security Standards:
     - NIST AI RMF (Govern, Map, Measure, Manage)
     - NIST 800-63B (Authentication)
 
-Version: 2.4.2
+Version: 2.4.3
 """
 
-__version__ = "2.4.2"
+__version__ = "2.4.3"
 __author__ = "ASCEND by OW-AI"
 
 # Core client (v2.1)
